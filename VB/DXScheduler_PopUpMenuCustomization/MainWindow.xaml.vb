a698daea1469a1ef6d55870c9c4d2e2a87683f97
DevExpressExampleBot <fakeEmail>
2023-11-21 15:12:17 +0400

    Source auto update [skip ci]

--- VB/DXScheduler_PopUpMenuCustomization/MainWindow.xaml.vb
+++ VB/DXScheduler_PopUpMenuCustomization/MainWindow.xaml.vb
@@ -1,21 +1,22 @@
-﻿Imports DevExpress.Xpf.Bars
+Imports DevExpress.Xpf.Bars
 Imports System.Windows
 Imports DevExpress.Xpf.Scheduling
 
 Namespace DXScheduler_PopUpMenuCustomization
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
         Private Sub allDayCheck_CheckedChanged(ByVal sender As Object, ByVal e As ItemClickEventArgs)
-            Dim selectedItem As AppointmentItem = scheduler.SelectedAppointments(0)
-            selectedItem.AllDay = If(allDayCheck.IsChecked = True, True, False)
+            Dim selectedItem As AppointmentItem = Me.scheduler.SelectedAppointments(0)
+            selectedItem.AllDay = If(Me.allDayCheck.IsChecked = True, True, False)
         End Sub
     End Class
 End Namespace
